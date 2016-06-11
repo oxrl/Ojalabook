2a9f612aed632edf860d4dc52d1f9a6036d7e3f8
oxrl <o.romero.lopez@gmail.com>
2016-06-10 19:58:00 -0700

    Generando vistas con Devise

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ gem 'rails', '4.2.6'
 gem 'sqlite3'
 
 gem 'devise'
+
+gem 'protected_attributes'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  attr_accessible :primer_nombre, :primer_apellido, :nombre_perfil,
+                  :email, :encrypted_password
 end
